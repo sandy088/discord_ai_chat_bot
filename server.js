145f5d9755edb7013c3a722acfd6e85064a0458a
Sandeep <sandeeppakho55@gmail.com>
2023-11-14 17:30:03 +0530

    FIX: Deploy Health Check Route

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const { Client, GatewayIntentBits } = require('discord.js') ;
 const { poemChat } = require('./services/discord/Chat');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageTyping] });
 
+const express = require('express');
+const app = express();
 
+app.use(express.json());    
 
 require('dotenv').config();
 
@@ -11,3 +14,12 @@ require('dotenv').config();
 client.on('messageCreate', poemChat)
 
 client.login(process.env.DISCORD_TOKEN);
+
+
+app.get('/', (req, res) => {
+    res.json({health: "Hey I am a Discord bot and running"})
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started")
+})
